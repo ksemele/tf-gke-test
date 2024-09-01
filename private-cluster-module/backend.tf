24642950f78e70c05e7e493d2810a71bef1819c8
Aleksei Krugliak <aleksei.krugliak@altenar.com>
2024-09-01 15:25:36 +0400

    comment backend.tf for default

--- private-cluster-module/backend.tf
+++ private-cluster-module/backend.tf
@@ -1,10 +1,10 @@
-# Optionallly you can use bucket as backend
-# If you would to do that - uncomment code below and follow instructions in README.md
+# # Optionallly you can use bucket as backend
+# # If you would to do that - uncomment code below and follow instructions in README.md
 
-terraform {
-  backend "gcs" {
-    bucket      = var.bucket_name
-    prefix      = var.bucket_prefix
-    credentials = var.bucket_credentials_json
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket      = var.bucket_name
+#     prefix      = var.bucket_prefix
+#     credentials = var.bucket_credentials_json
+#   }
+# }
